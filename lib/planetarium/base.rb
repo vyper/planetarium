dd271bd13fe6acbf8dfb62e63f8a155d37d713d1
Leonardo Saraiva <vyper@maneh.org>
2011-02-11 18:26:37 -0200

    Bug correction

--- lib/planetarium/base.rb
+++ lib/planetarium/base.rb
@@ -4,28 +4,34 @@ module Planetarium
       YAML::load_file(CONFIG_FILE)
     end
     
+    def self.config=(options)
+      config = self.config
+
+      config["name"]        = options[:name]        if !options[:name].nil?
+      config["template"]    = options[:template]    if !options[:template].nil?    # TODO check template exists
+      config["export_path"] = options[:export_path] if !options[:export_path].nil? # TODO check path exists
+
+      self.write_config(config)
+    end
+
     def self.add url
       config = self.config
       config["urls"] << url
-      self.config = config
+
+      self.write_config(config)
     end
     
     def self.del url
       config = self.config
       config["urls"].delete url
-      self.config = config
+      self.write_config(config)
     end
-
-    def self.config=(options)
-      config = self.config
-      
-      config["name"]        = options[:name]        if !options[:name].nil?
-      config["template"]    = options[:template]    if !options[:template].nil?    # TODO check template exists
-      config["export_path"] = options[:export_path] if !options[:export_path].nil? # TODO check path exists
-      
-      File.open(CONFIG_FILE, "w+") do |f|
-        f << YAML::dump(config)
+    
+    private
+      def self.write_config config
+        File.open(CONFIG_FILE, "w+") do |f|
+          f << YAML::dump(config)
+        end
       end
-    end
   end
 end
