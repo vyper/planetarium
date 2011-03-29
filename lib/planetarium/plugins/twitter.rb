83c7e66c68c6857765c44420ae221b385ecadfab
Leonardo Saraiva <vyper@maneh.org>
2011-03-29 19:52:06 -0300

    adding parser of @user, #hashtag and url in plugin twitter

--- lib/planetarium/plugins/twitter.rb
+++ lib/planetarium/plugins/twitter.rb
@@ -5,12 +5,19 @@ module Planetarium
 
       def search(options)
         list = []
+
+        er_url     = /(http:\/\/([a-zA-Z.\/0-9]+))/
+        er_hashtag = /#([a-zA-Z0-9]+)/
+        er_user    = /@([a-zA-Z0-9_]+)/
         
         twitter = Feedzirra::Feed.fetch_and_parse("http://search.twitter.com/search.atom?q=from%3A#{options[:from]}")
         twitter.entries.each do |t|
           tweet = Tweet.new
           tweet.from_user   = t.author
           tweet.text        = t.title
+          tweet.text        = tweet.text.gsub(er_url,     '<a href="\1" target="_blank">\2</a>')
+          tweet.text        = tweet.text.gsub(er_hashtag, '<a href="http://twitter.com/search?q=%23\1">#\1</a>')
+          tweet.text        = tweet.text.gsub(er_user,    '<a href="http://twitter.com/\1">@\1</a>')
           tweet.created_at  = t.published
           list << tweet
         end
